77796882a8c5413df76a5203dfe5fc54a9f88aed
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-03-12 20:05:26 -0300

    first commit

---
+++ Codificadores/CodificaByDouglasPacheco.java
@@ -0,0 +1,31 @@
+package Codificadores;
+
+class CodificaByDouglasPacheco implements Codifica {
+
+    @Override
+    public String getNomeAutor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        // TODO Auto-generated method stub
+        //teste
+        return null;
+    }
+
+    @Override
+    public String codifica(String str) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+}
