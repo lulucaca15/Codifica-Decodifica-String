9a81956a6efb4945716ad77670e2e773872af006
p-karol <priscilla.karolczak@gmail.com>
2020-03-22 22:57:24 +0000

    criada classe Codifica11109498

---
+++ Codificadores/Codifica11109498.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codificadores;
+
+/**
+ *
+ * @author p-karol
+ */
+public class Codifica11109498 implements Codifica{
+    
+    // Retorna o autor do codificador
+    String getNomeAutor(){
+        return "Priscilla de Azevedo Karolczak";
+    }
+
+    // Retorna a matrícula do autor do codificador
+    String getMatriculaAutor(){
+        return "11109498";
+    }
+
+    // Recebe um string e retorna o correspondente codificado
+    String codifica(String str){
+        
+    }
+
+    // Recebe um string codificado e retorna o correspondente decodificado
+    String decodifica(String str){
+        
+    }
+    
+}
