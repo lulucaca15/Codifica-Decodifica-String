e4b341471f4430aeacb8e9fe7d8c345ec1855359
joao <joao.narciso@acertsis.com.br>
2020-08-24 18:12:38 -0300

    metodos para codificar e decodificar strings base64

--- Codificadores/Codifica162041214.java
+++ Codificadores/Codifica162041214.java
@@ -1,5 +1,20 @@
 package Codificadores;
+import java.util.Base64;
 
 public class Codifica162041214 implements Codifica {
 
+ Base64.Encoder codificador = Base64.getEncoder();
+  Base64.Decoder decodifcar = Base64.getDecoder();
+
+    @Override
+    public String codifica(String str) {
+      String codificado = codificador.encodeToString(str.getBytes());
+      return codificado;
+    }
+  
+    @Override
+    public String decodifica(String str) {
+      String decodificado = new String(decodificador.decode(str));
+      return decodificado;
+    }
 }
