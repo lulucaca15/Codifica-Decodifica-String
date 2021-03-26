16cc81e7dd86e518ac39618ba4333860cb7754fc
Bruno Bueno <brunohebueno@gmail.com>
2021-03-26 18:58:55 -0300

    Adiciionado codificador 17120081

---
+++ Codificadores/Codifica17120081.java
@@ -0,0 +1,51 @@
+package Codificadores;
+
+public class Codifica17120081 implements Codifica {
+    private static final String tabela = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvw$%¨%#";
+    private static final int deslc = 7;
+
+    @Override
+    public String codifica(String str) {
+        String codificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                codificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        codificada += tabela.charAt(i + deslc);
+                    }
+                }
+            }
+        }
+        return codificada;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        String decodificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                decodificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        decodificada += tabela.charAt(i - deslc);
+                    }
+                }
+            }
+        }
+        return decodificada;
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        return "17120081";
+    }
+
+    @Override
+    public String getNomeAutor() {
+        return "Bruno Henrique Bueno";
+    }
+
+}
